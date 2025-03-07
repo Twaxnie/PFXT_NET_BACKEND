8ff93b405ea51d34b49802f1aef7a3de3f57c7a3
Twaxnie <twaxnie@gmail.com>
2025-03-07 17:37:54 -0500

    feat: add get and post for users

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function start() {
   const PORT = process.env.PORT || 5002;
   const app = await NestFactory.create(AppModule);
 
+  const config = new Document();
+
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 
